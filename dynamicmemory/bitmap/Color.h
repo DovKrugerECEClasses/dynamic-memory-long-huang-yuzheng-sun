1d3c515300928f5ccc0927559264e7b36ecc369b
YuzhengSun <ysun58@stevens.edu>
2018-11-08 01:53:10 -0500

    headerfileColor

--- dynamicmemory/bitmap/Color.h
+++ dynamicmemory/bitmap/Color.h
@@ -13,19 +13,19 @@ public:
     Color(uint8_t r, uint8_t g, uint8_t b){
         rgba = (r<<24) + (g<<16) + (b<<8) + 255;
     }
-    
+
     Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a){
         rgba = (r<<24) + (g<<16) + (b<<8) + a;
     }
     //TODO:	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha) {}
 
     string getColor(){
-        string color = "white";
-        if(rgba == 0xFF0000FF){color = "red";}
-        else if(rgba == 0x00FF00FF){color = "green";}
-        else if(rgba == 0x000000FF){color = "black";}
-        else if(rgba == 0x0000FFFF){color = "blue";}
-        else if(rgba == 0xFFFFFFFF){color = "white";}
+        string color = "WHITE";
+        if(rgba == 0xFF0000FF){color = "RED";}
+        else if(rgba == 0x00FF00FF){color = "GREEN";}
+        else if(rgba == 0x000000FF){color = "BLACK";}
+        else if(rgba == 0x0000FFFF){color = "BLUE";}
+        else if(rgba == 0xFFFFFFFF){color = "WHITE";}
 
         return color;
     }
