815a5aac5a31e04c1ab3e9b246e7fe9e1542a213
YuzhengSun <ysun58@stevens.edu>
2018-11-07 23:25:40 -0500

    headerfileColor

--- dynamicmemory/bitmap/Color.h
+++ dynamicmemory/bitmap/Color.h
@@ -11,12 +11,12 @@ public:
 	//TODO:	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha) {}
 
 	char getColor(){
-	    char color = 'white';
-	    if(rgba == 0xFF0000FF):{color = red;}
-	    else if(rgba == 0x00FF00FF):{color = green;}
-	    else if(rgba == 0x000000FF):{color = black;}
-	    else if(rgba == 0x0000FFFF):{color = blue;}
-	    else if(rgba == 0xFFFFFFFF):{color = white;}
+	    string color = "white";
+	    if(rgba == 0xFF0000FF):{color = "red";}
+	    else if(rgba == 0x00FF00FF):{color = "green";}
+	    else if(rgba == 0x000000FF):{color = "black";}
+	    else if(rgba == 0x0000FFFF):{color = "blue";}
+	    else if(rgba == 0xFFFFFFFF):{color = "white";}
 
 	    return color;
     }
